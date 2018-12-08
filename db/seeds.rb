8ffd781009895296a4b2306f887064c117b38692
Diego Rodriguez <diego.f.rodriguezh@gmail.com>
2018-12-08 12:48:31 +1100

    implemented simple seed.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Link.delete_all
+
+Link.create([{
+   url: "test.com.au"
+}])
+
+
